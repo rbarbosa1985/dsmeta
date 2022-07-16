433fc47a753819d47d7e9f84b88720b0a743a951
Roberto dos Santos Barbosa <betoanselmo@gmail.com>
2022-07-16 14:55:19 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>Bom dia!</h1>
+    <>
+      <h1>Bom dia!</h1>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+      <NotificationButton/>
+    </>
   )
 }
 
