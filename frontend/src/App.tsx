efbdf480d57b9df90650a9aee7c8471e88325200
Roberto dos Santos Barbosa <betoanselmo@gmail.com>
2022-07-16 15:53:00 -0300

    Other Commponets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Bom dia!</h1>
-      <NotificationButton/>
-      <NotificationButton/>
-      <NotificationButton/>
-      <NotificationButton/>
-      <NotificationButton/>
+      <Header/>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
