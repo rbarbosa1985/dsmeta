5a8746f4c0946539eccdb83c9e07f405201c27bc
Roberto dos Santos Barbosa <betoanselmo@gmail.com>
2022-07-16 17:54:46 -0300

    API test

---
+++ backend/src/main/java/com/devsuprior/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuprior.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuprior.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
