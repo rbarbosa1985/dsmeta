e5331c35489145c8c0be212892ea9fd073cfe7f1
Roberto dos Santos Barbosa <betoanselmo@gmail.com>
2022-07-16 23:01:25 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
+
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
+
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+  id:  number;
+}
+
+function NotificationButton( {id} : Props) {
+  
+  function handleClick (id: number){
+    axios(`${BASE_URL}/sales/${id}/notification`).then();
+  }
+
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(id)}>
       <img src={icon}></img>
     </div>
   )
