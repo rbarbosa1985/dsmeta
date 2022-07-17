474ef013fdf98031a139b6849bbdc620c8d0a1d1
Roberto dos Santos Barbosa <betoanselmo@gmail.com>
2022-07-16 23:04:47 -0300

    Toat

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
-import SalesCard from "./components/SalesCard"
+import { ToastContainer } from 'react-toastify';
+
+import Header from "./components/Header";
+import NotificationButton from "./components/NotificationButton";
+import SalesCard from "./components/SalesCard";
+
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+      <ToastContainer/>
       <Header/>
       <main>
         <section id="sales">
